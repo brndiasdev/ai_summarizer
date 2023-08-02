c86a141af17fd2d67c368446ea1806eaf0fd71a2
Bruno Dias <62982430+brndias1@users.noreply.github.com>
2023-08-01 21:43:05 -0300

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import copy from "./copy.svg";
+import logo from "./logo.svg";
+import tick from "./tick.svg";
+import brevis from "./brevis.svg";
+import bookgif from "./bookgif.gif";
+
+export { linkIcon, loader, copy, logo, tick, brevis, bookgif };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import { brevis, bookgif } from "../assets";
+
+const Hero = () => {
+	return (
+		<header className='w-full flex justify-center items-center flex-col'>
+			<nav className='flex justify-between items-center w-full mb-10 pt-3'>
+				<img
+					src={brevis}
+					alt='sumz_logo'
+					className='w-28 object-contain'
+				/>
+
+				<img
+					src={bookgif}
+					alt=''
+					height={45}
+					width={60}
+				/>
+				<button
+					type='button'
+					onClick={() =>
+						window.open("https://github.com/TidbitsJS/Summize", "_blank")
+					}
+					className='black_btn bg-gradient-to-r from-purple-400 to-purple-600'
+				>
+					Entrar
+				</button>
+			</nav>
+
+			<h1 className='head_text'>
+				Resuma seus Artigos <br className='max-md:hidden' />
+				<span className='purple_gradient'>Brevis GPT-4</span>
+			</h1>
+			<h2 className='desc'>
+				Simplifique sua leitura com o{" "}
+				<span className='text-extrabold text-purple-800'>Brevis</span>, um
+				resumidor de artigos que transforma artigos longos em resumos claros e
+				concisos
+			</h2>
+		</header>
+	);
+};
+
+export default Hero;
